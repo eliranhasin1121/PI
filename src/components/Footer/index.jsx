7b366406ffd7ceb4b2f2d0a6c6d710d78058ae32
Eliran Hasin <eliranh@rapyd.net>
2021-06-26 21:59:39 +0300

    WIP

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -14,6 +14,7 @@ export default function Footer({history}){
 	const isHE = pathname.includes('opportunities');
 
     return(
+		<>
         <FooterContainer>
 		 <FooterContentContainer>
 
@@ -61,6 +62,15 @@ export default function Footer({history}){
     }
     </FooterContentContainer>
 	  </FooterContainer>
+	  {isHE &&   
+	<FooterText>
+		<span>
+		המידע באתר אינו מהווה הצעה להשקעה בקרן או הזמנה לקבלת הצעות להשקעה בקרן, אינו מהווה בסיס לקבלת החלטות השקעה, ואינו מהווה המלצה ו/או חוות דעת ואינו תחליף לשיקול דעת של משקיע פוטנציאלי. כמו כן האמור כאן אינו מהווה תחליף לייעוץ מקצועי בידי בעל רישיון המתחשב בצרכי הלקוח וכן אינו מהווה תחליף לייעוץ מס. האמור באתר זה אינו מהווה הצעה של ניירות ערך. ניירות הערך של הקרנות השונות אינם רשומים ולא ירשמו למסחר והם מוצעים למשקיעים בדרך של הצעה פרטית למספר מוגבל של ניצעים כקבוע בחוק ני"ע. התקשרות בהסכם השקעה הינה לפי שיקול דעתו המוחלט של השותף הכללי.
+		</span>
+	</FooterText>
+}
+	  </>
+
     )
 }
 
@@ -90,6 +100,14 @@ color:#ffffff;
 const Logo = styled.img`
 width:42px;
 height:67px;
+`
+
+const FooterText = styled.p`
+	direction: rtl;
+    background: rgb(216, 216, 216);
+    padding: 25px;
+	margin-bottom : 0px;
+	color:white;
 `
 
 
